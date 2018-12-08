4653e9e772cd3b9d06eeee1b7d0346d757caa758
imvickykumar <vickythetechguy@gmail.com>
2018-12-08 18:35:42 +0530

    first commmit-input and output

---
+++ HelloWorld/helloworld.py
@@ -0,0 +1,23 @@
+# simple hello world
+
+print("hello world")
+
+
+# for loop in python using string;
+
+for y in "banana":
+    print(y)
+
+
+# for loop using an array;
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+# for loop using number;
+
+
+
+
